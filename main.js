6cf506e0c1baec133233c59d9d74ba0fb224780f
Patosaurio-o <pato.sepulveda@hotmail.es>
2020-12-22 10:04:52 -0300

    poke commit yo te elijo

---
+++ main.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+  for(let i=1; i <= 151; i++){
+    $('#poke-ball').append('<div id="poke-algo"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/'+i+'.png"></img><p>'+i+'</p></div>');
+
+  };
+});
